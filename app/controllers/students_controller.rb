9e3219b1b2bf09870ddb10009e608bd5e226aeb6
magic-onion <alexander.m.truong@gmail.com>
2018-11-01 11:05:37 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,8 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @students = Student.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'students', to: 'students#index'
-  
+
+  get '/students', to: 'students#index'
+  get '/students/:id', to: 'students#show'
+
 end
